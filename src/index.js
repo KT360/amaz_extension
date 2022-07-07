2b17f29df12c19ce36a7b0fd6677341053ce34b4
Astrid <kamtoastrid360@gmail.com>
2022-07-06 22:17:33 -0400

    pros

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ const myChart = new Chart(ctx,{
 });
 
 const axios = require('axios');
+const nlp = require('compromise');
 
 document.getElementById("start_button").addEventListener("click", displayInfo);
 
@@ -58,6 +59,8 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 });
 
 
+var reviews = null;
+
 function displayInfo()
 {
   if(asin_number)
@@ -73,8 +76,17 @@ function displayInfo()
     // make the http GET request to Rainforest API
     axios.get('https://api.rainforestapi.com/request', { params })
       .then(response => {
+        //Get top used adjectives
+        reviews = response.data.reviews;
+        let text = getPositiveText(reviews);
+        let first = getTextAdjectives(text); 
+        let next = getAdjFreq(first);
+        let after = filterWords(next);
+        updateTable(after);
+        //Get ratio of responses
         myChart.data.datasets[0].data =  getPercentage(response.data);
         myChart.update();
+        //update html
         document.getElementById('dual-ring').style.display = 'none';
         document.getElementById('success').style.display = 'inline-block';
         }).catch(error => {
@@ -110,4 +122,105 @@ function getPercentage(data)
 
 
     return [pos,hes,neg];
+}
+
+
+
+function getPositiveText(array)
+{
+    let new_arr = [];
+    let text = '';
+
+    array.forEach(element => {
+        if(element.rating > 3)
+        {
+            new_arr.push(element);
+        }
+    });
+
+    new_arr.forEach( element => {
+        text += element.body;
+    });
+
+    return text;
+}
+
+function getNegativeText(array)
+{
+    let new_arr = [];
+    let text = '';
+
+    array.forEach(element => {
+        if(element.rating < 3)
+        {
+            new_arr.push(element);
+        }
+    });
+
+    new_arr.forEach( element => {
+        text += element.body;
+    });
+
+    return text;
+}
+
+function getTextAdjectives(txt)
+{
+    let doc = nlp (txt);
+    let lower = doc.match('#Adjective').toLowerCase().text();
+    let final = lower.replace('!','').replace('.','').replace(',','');
+
+    return final;
+}
+
+function getAdjFreq(text)
+{
+    let str_arr = text.split(' ');
+    let dic = {};
+
+    for(let i = 0; i<str_arr.length; i++)
+    {
+        let word = str_arr[i];
+        if(dic[word])
+        {
+            dic[word] += 1;
+        }else{
+            dic[word] = 1;
+        }
+    }
+
+    return dic;
+
+}
+
+
+function filterWords(dict)
+{
+    let items = Object.keys(dict).map(
+        (key) => {return [key,dict[key]]}
+    );
+
+    items.sort(
+        (first,second) => {return second[1] - first[1]}
+    );
+
+    let next = items.map(
+        (item) => {
+            return item[0]
+        }
+    );
+    
+    let final = next.slice(0,5);
+
+    return final;
+}
+
+function updateTable(adj_array)
+{
+    let table = document.getElementById('Table');
+    adj_array.forEach((item) => {
+      let row = table.insertRow();
+      let adj = row.insertCell(0);
+      adj.innerHTML = item;
+    });
 }
